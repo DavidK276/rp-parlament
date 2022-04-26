4e12e5690b89797fc74490f132e0a10971e52d6e
David K <david.krchnavy@gmail.com>
2022-04-26 19:06:46 +0200

    add support for different user types

--- konto.php
+++ konto.php
@@ -5,17 +5,20 @@ include('functions.php');
 include('database.php');
 head(isset($_SESSION[SESSION_USER_ID]) ? 'Konto' : 'Prihlásenie');
 // overenie udajov treba robit skor ako sa spusti navbar, aby sa prihlasenie hned prejavilo
-if (isset($_POST['submit'])) {
-
-    if (verify_user($mysqli, $email, $_POST['pswd'])) {
+if (isset($_POST['login'])) {
+    $verify = verify_user($mysqli, $_POST['email'], $_POST['pswd']);
+    if ($verify == 1) {
         // spravne udaje
-        $user = select_user($mysqli, $_POST['email']);
-        $_SESSION[SESSION_USER_ID] = $user['id'];
-        $_SESSION[SESSION_USER_EMAIL] = $user['email'];
+        $_SESSION[SESSION_USER_ID] = select_poslanec($mysqli, $_POST['email']);
+        $_SESSION[SESSION_USER_ROLE] = 'poslanec';
+    }
+    else if($verify == 2) {
+        $_SESSION[SESSION_USER_ID] = select_admin($mysqli, $_POST['email']);
+        $_SESSION[SESSION_USER_ROLE] = 'admin';
     }
     else {
         // nespravne udaje
-        $password_incorrect = false;
+        $password_incorrect = true;
     }
 }
 include('navbar.php');
@@ -25,7 +28,7 @@ if (!isset($_SESSION[SESSION_USER_ID])) { ?>
         <div class="col-md-4">
             <div class="container">
                 <h2>Prihlásenie do parlamentu</h2>
-                <?php if (isset($password_incorrect) && !$password_incorrect) echo '<p class="text-danger m-0">Nesprávne meno alebo heslo.</p>' ?>
+                <?php if (isset($password_incorrect) && $password_incorrect) echo '<p class="text-danger m-0">Nesprávne meno alebo heslo.</p>' ?>
                 <form method="post">
                     <div class="mb-3 mt-3">
                         <label for="email" class="form-label">Email:</label>
@@ -40,7 +43,7 @@ if (!isset($_SESSION[SESSION_USER_ID])) { ?>
                             <input class="form-check-input" type="checkbox" name="remember"> Zapamätať prihlásenie
                         </label>
                     </div>
-                    <button type="submit" name="submit" class="btn btn-primary">Prihlásiť</button>
+                    <button type="submit" name="login" class="btn btn-primary">Prihlásiť</button>
                 </form>
             </div>
         </div>
