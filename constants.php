d90a64d476057df4bb76e4afa6f58a8fad6f1db9
David K <david.krchnavy@gmail.com>
2022-05-02 18:32:21 +0200

    update constants

--- constants.php
+++ constants.php
@@ -1,3 +1,5 @@
 <?php
-const SESSION_USER_ID = 'session_user_id';
+const SESSION_USER = 'session_user_id';
 const SESSION_USER_ROLE = 'session_user_role';
+const ROLE_ADMIN = 'role_admin';
+const ROLE_POSLANEC = 'role_poslanec';
