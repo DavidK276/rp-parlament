b97e1e56d61d0db576d4b2a95af0b98fa6e6131b
david <david.krchnavy@gmail.com>
2022-05-03 11:10:39 +0200

    exit on database error

--- database.php
+++ database.php
@@ -2,6 +2,7 @@
 $mysqli = new mysqli('127.0.0.1', 'david', 'heslo', 'parlament');
 if ($mysqli -> connect_errno) {
     echo '<p class="text-danger">Nepodarilo sa nadviazať spojenie s databázou.</p>';
+    exit();
 }
 else {
     $mysqli -> set_charset('utf8mb4');
