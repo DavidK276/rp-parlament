9bf3ff080c168d28cd1a11ca25671d51d3be0d3f
David K <david.krchnavy@gmail.com>
2022-05-05 12:26:27 +0200

    add new constants

--- constants.php
+++ constants.php
@@ -1,5 +1,11 @@
 <?php
 const SESSION_USER = 'session_user_id';
 const SESSION_USER_ROLE = 'session_user_role';
-const ROLE_ADMIN = 'role_admin';
-const ROLE_POSLANEC = 'role_poslanec';
+
+const ROLE_ADMIN = 1;
+const ROLE_POSLANEC = 2;
+
+const SUCCESS = 0;
+const ERROR_USER_EXISTS = -3;
+const ERROR_USER_NOT_FOUND = -2;
+const ERROR_UNKNOWN = -1;
