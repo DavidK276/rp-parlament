8ab2f873b4c85fa84556d7cfe6c06009019161fa
David K <david.krchnavy@gmail.com>
2022-05-05 12:26:57 +0200

    add docstrings

--- functions.php
+++ functions.php
@@ -1,21 +1,32 @@
 <?php
 date_default_timezone_set('Europe/Bratislava');
 
-function partition(Array $list, $p) {
-    $listlen = count($list);
-    $partlen = floor($listlen / $p);
-    $partrem = $listlen % $p;
+/**
+ * Partitions array into a specified number of arrays
+ * @param array $arr array to partition
+ * @param int $p number of arrays to produce
+ * @return array n+1 dismensional array
+ */
+function partition(Array $arr, int $p): array {
+    $arrlen = count($arr);
+    $partlen = floor($arrlen / $p);
+    $partrem = $arrlen % $p;
     $partition = array();
     $mark = 0;
     for($px = 0; $px < $p; $px ++) {
         $incr = ($px < $partrem) ? $partlen + 1 : $partlen;
-        $partition[$px] = array_slice($list, $mark, $incr);
+        $partition[$px] = array_slice($arr, $mark, $incr);
         $mark += $incr;
     }
     return $partition;
 }
 
-function head($title = 'Úvod'): void
+/**
+ * Produces a html head with the specified title
+ * @param string $title
+ * @return void
+ */
+function head(string $title = 'Úvod'): void
 { ?>
     <!DOCTYPE html>
     <html lang="sk">
@@ -23,6 +34,8 @@ function head($title = 'Úvod'): void
     <title><?php echo $title . " - Fiktívny Parlament"; ?></title>
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="style.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 <body>
 <header class="container-fluid bg-primary bg-gradient text-white">
@@ -31,12 +44,24 @@ function head($title = 'Úvod'): void
 </header>
 <?php }
 
-function display_error($error): void
+/**
+ * Displays an error message with the specified test to the user
+ * @param string $error_text
+ * @return void
+ */
+function display_error(string $error_text): void
 {
-    echo "<div class=\"container\"><div class=\"row\"><div class=\"col-md-12\"><h3>$error</h3></div></div></div>";
+    echo "<div class=\"container\"><div class=\"row\"><div class=\"col-md-12\"><h3>$error_text</h3></div></div></div>";
 }
 
-function verify_user($mysqli, $email, $pswd): int
+/**
+ * verifies the user login with the database
+ * @param mysqli $mysqli
+ * @param string $email
+ * @param string $pswd
+ * @return int -1 on error, role of the user on success
+ */
+function verify_user(mysqli $mysqli, string $email, string $pswd): int
 {
     if (!$mysqli->connect_errno) {
         $email = filter_var($email, FILTER_SANITIZE_EMAIL);
@@ -48,7 +73,7 @@ function verify_user($mysqli, $email, $pswd): int
         if ($result->num_rows > 0) {
             $row = $result->fetch_assoc();
             $result->free();
-            if (password_verify($pswd, $row['heslo'])) return 1;
+            if (password_verify($pswd, $row['heslo'])) return ROLE_POSLANEC;
             else return -1; // nespravne heslo
         } else {
             $stmt = $mysqli->prepare('SELECT heslo FROM osobne_udaje, admin WHERE admin.id_udaje=osobne_udaje.id AND osobne_udaje.email=?');
@@ -59,7 +84,7 @@ function verify_user($mysqli, $email, $pswd): int
             if ($result->num_rows > 0) {
                 $row = $result->fetch_assoc();
                 $result->free();
-                if (password_verify($pswd, $row['heslo'])) return 2;
+                if (password_verify($pswd, $row['heslo'])) return ROLE_ADMIN;
                 else return -1; // nespravne heslo
             }
         }
@@ -67,7 +92,32 @@ function verify_user($mysqli, $email, $pswd): int
     return -1;
 }
 
-function select_osobne_udaje($mysqli, $email): array
+/**
+ * queries the database to find if the user exists
+ * @param mysqli $mysqli
+ * @param string $email an email of the user to query
+ * @return bool
+ */
+function user_exists(mysqli $mysqli, string $email): bool
+{
+    if (!$mysqli->connect_errno) {
+        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        $stmt = $mysqli->prepare('SELECT email FROM osobne_udaje WHERE email=?');
+        $stmt->bind_param('s', $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $stmt->close();
+        if ($result->num_rows > 0) {
+            $result->free();
+            return true;
+        }
+        $result->free();
+        return false;
+    }
+    return false;
+}
+
+function select_osobne_udaje(mysqli $mysqli, string $email): array
 {
     if ((!empty($email) || !empty($user_id)) && !$mysqli->connect_errno) {
         $email = filter_var($email, FILTER_SANITIZE_EMAIL);
@@ -85,7 +135,7 @@ function select_osobne_udaje($mysqli, $email): array
     return [];
 }
 
-function select_poslanec($mysqli, $email): array
+function select_poslanec(mysqli $mysqli, string $email): array
 {
     if ((!empty($email) || !empty($user_id)) && !$mysqli->connect_errno) {
         $email = filter_var($email, FILTER_SANITIZE_EMAIL);
@@ -95,7 +145,7 @@ function select_poslanec($mysqli, $email): array
     return [];
 }
 
-function select_admin($mysqli, $email): array
+function select_admin(mysqli $mysqli, string $email): array
 {
     if ((!empty($email) || !empty($user_id)) && !$mysqli->connect_errno) {
         $email = filter_var($email, FILTER_SANITIZE_EMAIL);
@@ -105,7 +155,7 @@ function select_admin($mysqli, $email): array
     return [];
 }
 
-function select_user($stmt, $email): array
+function select_user(mysqli_stmt $stmt, string $email): array
 {
     $stmt->bind_param('s', $email);
     $stmt->execute();
@@ -118,7 +168,7 @@ function select_user($stmt, $email): array
     } else return []; // pouzivatel sa nenasiel
 }
 
-function insert_osobne_udaje($mysqli, $email, $meno, $priezvisko, $adresa): int
+function insert_osobne_udaje(mysqli $mysqli, string $email, string $meno, string $priezvisko, string $adresa): int
 {
     if (!$mysqli->connect_errno) {
         $stmt = $mysqli->prepare('INSERT INTO osobne_udaje(email, meno, priezvisko, adresa) VALUES(?, ?, ?, ?)');
@@ -129,41 +179,45 @@ function insert_osobne_udaje($mysqli, $email, $meno, $priezvisko, $adresa): int
     return -1;
 }
 
-// TODO: osetrit ak email existuje
-function insert_poslanec($mysqli, $poslanec): bool
+function insert_poslanec(mysqli $mysqli, array $poslanec): int
 {
+    if (user_exists($mysqli, $poslanec['email'])) return ERROR_USER_EXISTS;
     if (!$mysqli->connect_errno) {
         $udaje = insert_osobne_udaje($mysqli, $poslanec['email'], $poslanec['meno'], $poslanec['priezvisko'], $poslanec['adresa']);
         if ($udaje > 0) {
             $stmt = $mysqli->prepare('INSERT INTO poslanec(id_udaje, specializacia, titul, heslo) VALUES(?, ?, ?, ?)');
+            $pswd = password_hash($poslanec['heslo'], PASSWORD_DEFAULT);
             $stmt->bind_param('isss', $udaje, $poslanec['specializacia'],
-                $poslanec['titul'], password_hash($poslanec['heslo'], PASSWORD_DEFAULT));
+                $poslanec['titul'], $pswd);
             $stmt->execute();
-            if (!$stmt->errno) return true;
+            if (!$stmt->errno) return SUCCESS;
         }
     }
-    return false;
+    return ERROR_UNKNOWN;
 }
 
-function insert_admin($mysqli, $admin): bool
+function insert_admin(mysqli $mysqli, array $admin): bool
 {
+    if (user_exists($mysqli, $admin['email'])) return ERROR_USER_EXISTS;
     if (!$mysqli->connect_errno) {
         $udaje = insert_osobne_udaje($mysqli, $admin['email'], $admin['meno'], $admin['priezvisko'], $admin['adresa']);
         if ($udaje > 0) {
             $stmt = $mysqli->prepare('INSERT INTO admin(id_udaje, id_previerka, heslo) VALUES(?, ?, ?)');
-            $stmt->bind_param('iis', $udaje, $admin['id_previerka'], password_hash($admin['heslo'], PASSWORD_DEFAULT));
+            $pswd = password_hash($admin['heslo'], PASSWORD_DEFAULT);
+            $stmt->bind_param('iis', $udaje, $admin['id_previerka'], $pswd);
             $stmt->execute();
-            if (!$stmt->errno) return true;
+            if (!$stmt->errno) return SUCCESS;
         }
     }
-    return false;
+    return ERROR_UNKNOWN;
 }
 
-function update_osobne_udaje($mysqli, $email, $meno, $priezvisko, $adresa)
+function update_osobne_udaje(mysqli $mysqli, string $email, string $meno, string $priezvisko, string $adresa): int
 {
     // TODO: skontrolovat spravanie pri update emailu na duplicitny
     if (!$mysqli->connect_errno) {
         $old_udaje = select_osobne_udaje($mysqli, $email);
+        if (user_exists($mysqli, $email) && $old_udaje['email'] != $email) return ERROR_USER_EXISTS;
         $stmt = $mysqli->prepare("UPDATE osobne_udaje SET email=?,meno=?,priezvisko=?,adresa=? WHERE id=?");
         $stmt->bind_param('ssssi', empty($email) ? $old_udaje['email'] : $email,
             empty($admin['first_name']) ? $old_udaje['meno'] : $meno,
@@ -171,45 +225,49 @@ function update_osobne_udaje($mysqli, $email, $meno, $priezvisko, $adresa)
             empty($admin['address']) ? $old_udaje['adresa'] : $adresa,
             $old_udaje['id']);
         $stmt->execute();
-        if ($stmt->affected_rows > 0) return true;
+        if ($stmt->affected_rows > 0) return SUCCESS;
     }
-    return false;
+    return ERROR_UNKNOWN;
 }
 
-function update_admin($mysqli, $admin): bool
+function update_admin(mysqli $mysqli, array $admin): int
 {
     if (!$mysqli->connect_errno) {
         $old_data = select_admin($mysqli, $admin['email']);
-        update_osobne_udaje($mysqli, $admin['email'], $admin['first_name'], $admin['last_name'], $admin['address']);
+        $update_result = update_osobne_udaje($mysqli, $admin['email'], $admin['first_name'], $admin['last_name'], $admin['address']);
+        if ($update_result == ERROR_USER_EXISTS) return ERROR_USER_EXISTS;
         $stmt = $mysqli->prepare("UPDATE admin SET id_previerka=?,heslo=? WHERE id=?");
+        $pswd = password_hash($admin['heslo'], PASSWORD_DEFAULT);
         $stmt->bind_param('isi', empty($admin['id_previerka']) ? $old_data['id_previerka'] : $admin['id_previerka'],
-            empty($admin['heslo']) ? $old_data['heslo'] : password_hash($admin['heslo'], PASSWORD_DEFAULT),
+            empty($admin['heslo']) ? $old_data['heslo'] : $pswd,
             $old_data['id']);
         $stmt->execute();
-        if ($stmt->affected_rows > 0) return true;
+        if ($stmt->affected_rows > 0) return SUCCESS;
     }
-    return false;
+    return ERROR_UNKNOWN;
 }
 
-function update_poslanec($mysqli, $poslanec): bool
+function update_poslanec(mysqli $mysqli, array $poslanec): int
 {
     if (!$mysqli->connect_errno) {
         $old_data = select_poslanec($mysqli, $poslanec['email']);
-        update_osobne_udaje($mysqli, $poslanec['email'], $poslanec['first_name'], $poslanec['last_name'], $poslanec['address']);
+        $update_result = update_osobne_udaje($mysqli, $poslanec['email'], $poslanec['first_name'], $poslanec['last_name'], $poslanec['address']);
+        if ($update_result == ERROR_USER_EXISTS) return ERROR_USER_EXISTS;
         $stmt = $mysqli->prepare("UPDATE poslanec SET id_klub=?,id_previerka=?,specializacia=?,titul=?,heslo=? WHERE poslanec.id=?");
+        $pswd = password_hash($poslanec['heslo'], PASSWORD_DEFAULT);
         $stmt->bind_param('iisssi', empty($poslanec['id_klub']) ? $old_data['id_klub'] : $poslanec['id_klub'],
             empty($poslanec['id_previerka']) ? $old_data['id_previerka'] : $poslanec['id_previerka'],
             empty($poslanec['specializacia']) ? $old_data['specializacia'] : $poslanec['specializacia'],
             empty($poslanec['titul']) ? $old_data['titul'] : $poslanec['titul'],
-            empty($poslanec['heslo']) ? $old_data['heslo'] : password_hash($poslanec['heslo'], PASSWORD_DEFAULT),
+            empty($poslanec['heslo']) ? $old_data['heslo'] : $pswd,
             $old_data['id']);
         $stmt->execute();
-        if ($stmt->affected_rows > 0) return true;
+        if ($stmt->affected_rows > 0) return SUCCESS;
     }
-    return false;
+    return ERROR_UNKNOWN;
 }
 
-function delete_admin($mysqli, $email): bool
+function delete_admin(mysqli $mysqli, string $email): int
 {
     if (!$mysqli->connect_errno) {
         $admin = select_admin($mysqli, $email);
@@ -220,13 +278,14 @@ function delete_admin($mysqli, $email): bool
             $stmt = $mysqli->prepare('DELETE FROM osobne_udaje WHERE email=?');
             $stmt->bind_param('s', $admin['email']);
             $stmt->execute();
-            if ($stmt->affected_rows > 0) return true;
+            if ($stmt->affected_rows > 0) return SUCCESS;
         }
+        return ERROR_USER_NOT_FOUND;
     }
-    return false;
+    return ERROR_UNKNOWN;
 }
 
-function delete_poslanec($mysqli, $email): bool
+function delete_poslanec(mysqli $mysqli, string $email): int
 {
     if (!$mysqli->connect_errno) {
         $poslanec = select_poslanec($mysqli, $email);
@@ -237,13 +296,19 @@ function delete_poslanec($mysqli, $email): bool
             $stmt = $mysqli->prepare('DELETE FROM osobne_udaje WHERE email=?');
             $stmt->bind_param('s', $poslanec['email']);
             $stmt->execute();
-            if ($stmt->affected_rows > 0) return true;
+            if ($stmt->affected_rows > 0) return SUCCESS;
         }
+        return ERROR_USER_NOT_FOUND;
     }
-    return false;
+    return ERROR_UNKNOWN;
 }
 
-function sanitise($input): string
+/**
+ * sanitises the user input
+ * @param string $input
+ * @return string
+ */
+function sanitise(string $input): string
 {
     return addslashes(trim(strip_tags($input)));
 }
@@ -251,7 +316,12 @@ function sanitise($input): string
 /* kontroluje meno (meno a priezvisko)
 vráti TRUE, ak celé meno ($input) obsahuje práve 1 medzeru, pred a za medzerou sú časti aspoň dĺžky 3 znaky
 */
-function verify_name($input): bool
+/**
+ * checks the validity of the name input by the user
+ * @param string $input
+ * @return bool true if input contains exactly one space and at least three characters before and after the space
+ */
+function verify_name(string $input): bool
 {
     $space = strpos($input, ' ');
     if (!$space) return false;
@@ -259,14 +329,24 @@ function verify_name($input): bool
     return ($space > 2 && (!str_contains($last_name, ' ')) && strlen($last_name) > 2);
 }
 
-function get_spec_values($mysqli): array
+/**
+ * retrieves all possible values of 'specializacia' from the database
+ * @param mysqli $mysqli
+ * @return string[]
+ */
+function get_spec_values(mysqli $mysqli): array
 {
     $type = $mysqli->query("SHOW COLUMNS FROM poslanec WHERE Field = 'specializacia'")->fetch_assoc()['Type'];
     preg_match("/^set\('(.*)'\)$/", $type, $matches);
     return explode("','", $matches[1]);
 }
 
-function get_all_poslanci($mysqli): array
+/**
+ * retrieves all data from table 'poslanci' and 'osobne_udaje' for display to the user
+ * @param mysqli $mysqli
+ * @return array[]
+ */
+function get_all_poslanci(mysqli $mysqli): array
 {
     if (!$mysqli->connect_errno) {
         $sql = "SELECT poslanec.id, titul, osobne_udaje.meno, priezvisko FROM osobne_udaje, poslanec WHERE poslanec.id_udaje=osobne_udaje.id";
