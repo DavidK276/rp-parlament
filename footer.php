8d05e41426e0a1132b3fc9c3b0582b30c50ce9f8
David K <david.krchnavy@gmail.com>
2022-05-12 11:13:57 +0200

    add quote

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
-<footer class="container-fluid bg-primary text-white mt-4">
-<div>Vytvoril David Krchňavý (ISIC: 461), 2022</div>
+<footer class="d-md-flex justify-content-between bg-primary text-white mt-4 px-4">
+    <div class="py-2">Vytvoril David Krchňavý (ISIC: 461), 2022</div>
+    <div class="py-2"><em><q>Web 1.0 by neexistoval bez PHP. Web 2.0 by bez PHP nebol taký ostražitý pred bezpečnostnými dierami.</q></em></div>
 </footer>
 
 </body>
