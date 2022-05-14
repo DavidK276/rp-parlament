82ca77bf71caaf9b21c1e3b8888ba12a38aff74c
david <david.krchnavy@gmail.com>
2022-05-14 16:27:57 +0200

    update description

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ include('navbar.php'); ?>
                             <li>Osobné údaje</li>
                             <li>Prihlásenie</li>
                             <li>Bezpečnostná previerka</li>
+                            <li>Poslanecký klub</li>
                         </ul>
                     </li>
                     <li class="list-group-item">
@@ -24,9 +25,16 @@ include('navbar.php'); ?>
                         <ul>
                             <li>Pridanie, úprava poslancov</li>
                             <li>Správa bezp. previerky</li>
+                            <li>Správa posl. klubov</li>
                         </ul>
                     </li>
-                    <!--                <li class="list-group-item"></li>-->
+                    <li class="list-group-item">
+                        <h5>Ostatní</h5>
+                        <ul>
+                            <li>Zobrazenie poslanov</li>
+                            <li>Filtrovanie podľa kritérii</li>
+                        </ul>
+                    </li>
                 </ul>
             </div>
         </div>
